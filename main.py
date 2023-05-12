7f0f40fb06fa960700ab8220606760eb206abdfb
AlejandroFdezGarcia <al.fernandezg.2020@alumnos.urjc.es>
2023-05-12 17:41:14 +0200

    Primera versión ej 1

--- main.py
+++ main.py
@@ -1,19 +1,62 @@
 import sqlite3
 import csv
 import pandas as pd
+from flask import Flask, render_template
+import matplotlib.pyplot as plt
 
-#Para poner los datos del csv en la Base de datos
+app = Flask(__name__)
 
-conn = sqlite3.connect('bd.db')
+@app.route('/top_ips/<int:x>')
+def top_ips(x):
+    con = sqlite3.connect('bd.db')
+    cur = con.cursor()
+    cur.execute(f"SELECT origin, COUNT(*) FROM alertas WHERE priority = 1 GROUP BY origin ORDER BY COUNT(*) DESC LIMIT {x}")
 
-cursor = conn.cursor()
-cursor.execute('''CREATE TABLE alertas
-                   (timestamp datetime, sid int, msg text, clasification text, priority int, protocol text, origin text, destination text, port int)''')
+    results = cur.fetchall()
+    con.close()
 
-with open('alerts.csv', newline='') as csvfile:
-    reader = csv.reader(csvfile)
-    for row in reader:
-        cursor.execute("INSERT INTO alertas (timestamp, sid, msg, clasification, priority, protocol, origin, destination, port) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", row)
+    ips = [result[0] for result in results]
+    counts = [result[1] for result in results]
 
-conn.commit()
-conn.close()
+    plt.bar(ips[:x], counts[:x])  # Limitar a los primeros x elementos
+    plt.xlabel('IPs de origen')
+    plt.ylabel('Número de incidencias')
+    plt.title(f'Top {x} IPs de origen con mayor número de incidencias (prioridad = 1)')
+
+    # Guardar el gráfico en un archivo
+    graph_file = 'static/graph.png'
+    plt.savefig(graph_file, format='png')
+    plt.close()
+
+    # Renderizar la plantilla HTML con la ruta al archivo del gráfico
+    return render_template('graph.html', graph_file=graph_file)
+
+
+@app.route('/top_devices/<int:x>')
+def top_devices(x):
+    con = sqlite3.connect('bd.db')
+    cur = con.cursor()
+    cur.execute(f"SELECT origin, COUNT(*) FROM alertas WHERE priority = 1 GROUP BY origin ORDER BY COUNT(*) DESC LIMIT {x}")
+
+    results = cur.fetchall()
+    con.close()
+
+    devices = [result[0] for result in results]
+    counts = [result[1] for result in results]
+
+    plt.bar(devices[:x], counts[:x])  # Limitar a los primeros x elementos
+    plt.xlabel('Dispositivos')
+    plt.ylabel('Número de incidencias')
+    plt.title(f'Top {x} dispositivos más vulnerables (prioridad = 1)')
+
+    # Guardar el gráfico en un archivo
+    graph_file = 'static/graph.png'
+    plt.savefig(graph_file, format='png')
+    plt.close()
+
+    # Renderizar la plantilla HTML con la ruta al archivo del gráfico
+    return render_template('graph.html', graph_file=graph_file)
+
+
+if __name__ == '__main__':
+    app.run()
